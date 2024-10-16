ca825be52ea64346f1ab7f5b0737889680ca6518
learoy-led <marketing@learoyled.com>
2024-10-16 12:29:58 +0200

    add home link to logo and fix home subtitles style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

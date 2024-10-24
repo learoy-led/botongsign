8733b0d9bc23e56d574c4065f02160c98b553b03
learoy-led <marketing@learoyled.com>
2024-10-24 15:41:06 +0200

    add begin of the route

--- src/app/shared/icons/icons.component.ts
+++ src/app/shared/icons/icons.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { HowElement, Sector, Solution } from '../../models/data.models';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-icons',
@@ -10,9 +10,16 @@ import { RouterModule } from '@angular/router';
   templateUrl: './icons.component.html',
   styleUrl: './icons.component.css'
 })
-export class IconsComponent {
+export class IconsComponent implements OnInit {
   @Input() iconElements: HowElement[] | Solution[] | Sector[] = [];
   @Input() isHidden: boolean = true;
   @Input() iconLink: string = '';
+  currentRoute = ''
+
+constructor(private router: Router) {}
+
+  ngOnInit() {
+this.currentRoute = this.router.url
+  }
 }
 

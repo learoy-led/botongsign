aee1cd8a8fc65fcb9cb8248fa9416642145de6fd
learoy-led <marketing@learoyled.com>
2024-10-24 15:34:40 +0200

    add links to icons

--- src/app/components/contact-banner/contact-banner.component.ts
+++ src/app/components/contact-banner/contact-banner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/button/button.component';
-import { NavigationEnd, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-banner',
--- src/app/shared/icons/icons.component.ts
+++ src/app/shared/icons/icons.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { HowElement, Sector, Solution } from '../../models/data.models';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-icons',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './icons.component.html',
   styleUrl: './icons.component.css'
 })
 export class IconsComponent {
   @Input() iconElements: HowElement[] | Solution[] | Sector[] = [];
   @Input() isHidden: boolean = true;
+  @Input() iconLink: string = '';
 }
+
